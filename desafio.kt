e3aaec0ceab9832c27c59e787651588b48b0cc96
Júllya Cabral <124018755+Vct-Jully@users.noreply.github.com>
2023-05-01 16:44:32 -0300

    atualizaç^\o- resolução de problemas

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,59 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// [Template no Kotlin Playground](https://pl.kotl.in/8md-t8bf1)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+val usuario: MutableList<String> = mutableListOf("Thales", "Bianca")
+val inscritos: List<String> = usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+ //foi criada uma função matricular para adcionar um novo usuário 
+ //a lista de inscritos retorna os nomes de todos os inscritos, incluindo novas matriculas
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(newUsuario: String){  
+    usuario.add(newUsuario) 
     }
+     
+fun getinscritos(): List<String> {
+    return inscritos
 }
+
+data class ConteudoEducacional(var nome: String, var atividades: Int, var horasDeDuracao: Int){
+    
+}
+
+
+data class Formacao(val nome: String, val listconteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+}
+
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    // criar lista de inscritos
+    matricular("Carla")
+    matricular("Theo")
+    println("Usuários Inscritos:")
+    
+    getinscritos().forEach {
+        i -> println("$i")
+    }
+    println("")
+    
+    
+    val curso1 = ConteudoEducacional("Princípios de Agilidade e Desenvolvimento de Software", 8, 15)
+    val curso2 = ConteudoEducacional("Dominando a Linguagem de Programação Kotlin", 6, 11)
+    val curso3 = ConteudoEducacional("Refinando Sua Técnica Com Desafios de Código em Kotlin", 5, 5)
+    val curso4 = ConteudoEducacional("Fortalecendo Seu Perfil Profissional", 4, 5)
+    
+    val listconteudos= mutableListOf<ConteudoEducacional>()
+    listconteudos.add(curso1)
+    listconteudos.add(curso2)
+    listconteudos.add(curso3)
+    listconteudos.add(curso4)
+
+    val formacaoKotlinExperience = Formacao("Kotlin Experience", listconteudos, Nivel.INTERMEDIARIO)
+   
+   println("${formacaoKotlinExperience.nome} - ${formacaoKotlinExperience.nivel}")   
+   listconteudos.forEach {
+        i -> println("$i")
+   }
 }
+
